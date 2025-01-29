df790f597dd07850ce6999ca94bdff8520b03e3c
Assa <codermasochist@gmail.com>
2025-01-29 17:10:21 +0600

    Update 3install.sh

--- 3install.sh
+++ 3install.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-# Получаем текущую рабочую директорию
 WORKING_DIR=$(pwd)
 
 runin() {
-	# Runs the arguments, piping stderr to logfile
-	{ "$@" 2>>"${WORKING_DIR}/hikka-install.log" || return $?; } | while read -r line; do
-		printf "%s\n" "$line" >>"${WORKING_DIR}/hikka-install.log"
+	{ "$@" 2>>"${WORKING_DIR}/heroku-install.log" || return $?; } | while read -r line; do
+		printf "%s\n" "$line" >>"${WORKING_DIR}/heroku-install.log"
 	done
 }
 
 runout() {
-	# Runs the arguments, piping stderr to logfile
-	{ "$@" 2>>"${WORKING_DIR}/hikka-install.log" || return $?; } | while read -r line; do
-		printf "%s\n" "$line" >>"${WORKING_DIR}/hikka-install.log"
+	{ "$@" 2>>"${WORKING_DIR}/heroku-install.log" || return $?; } | while read -r line; do
+		printf "%s\n" "$line" >>"${WORKING_DIR}/heroku-install.log"
 	done
 }
 
 errorin() {
-	cat "${WORKING_DIR}/hikka-install.log"
+	cat "${WORKING_DIR}/heroku-install.log"
 }
 errorout() {
-	cat "${WORKING_DIR}/hikka-install.log"
+	cat "${WORKING_DIR}/heroku-install.log"
 }
 
 SUDO_CMD=""
@@ -30,8 +27,6 @@ if [ ! x"$SUDO_USER" = x"" ]; then
 		SUDO_CMD="sudo -u $SUDO_USER "
 	fi
 fi
-
-##############################################################################
 
 clear
 clear
@@ -42,48 +37,33 @@ printf "\n\e[1;35;47m █▀█ █ █ █ █ █ █▀█ \e[0m"
 printf "\n\e[1;35;47m                   \e[0m"
 printf "\n\n\e[3;34;40m Installing...\e[0m\n\n"
 
-##############################################################################
-
 printf "\r\033[0;34mPreparing for installation...\e[0m"
 
-# Удалено создание и изменение владельца файла hikka-install.log
-
-if [ -d "Hikka/hikka" ]; then
-	cd Hikka || {
+if [ -d "Heroku/heroku" ]; then
+	cd Heroku || {
 		printf "\rError: Install git package and re-run installer"
 		exit 6
 	}
 	DIR_CHANGED="yes"
 fi
 if [ -f ".setup_complete" ]; then
-	# If hikka is already installed by this script
 	PYVER="3.10"
-	printf "\rExisting installation detected. Hikka is already installed.\n"
+	printf "\rExisting installation detected. Heroku is already installed.\n"
 fi
 
-##############################################################################
+echo "Preparing installation..." >"${WORKING_DIR}/heroku-install.log"
 
-echo "Preparing installation..." >"${WORKING_DIR}/hikka-install.log"
-
-# Удаление проверки операционной системы и установки пакетов, поскольку они уже скачаны
-
-PYVER="3.10" # Стандартная версия Python
-
-##############################################################################
+PYVER="3.10"
 
 printf "\r\033[K\033[0;32mPreparation complete!\e[0m"
 printf "\n\r\033[0;34mCloning repo...\e[0m"
 
-##############################################################################
-
-# shellcheck disable=SC2086
-${SUDO_CMD}rm -rf "${WORKING_DIR}/Hikka"
-# shellcheck disable=SC2086
-runout ${SUDO_CMD}git clone https://github.com/hikariatama/Hikka/ "${WORKING_DIR}/Hikka" || {
+${SUDO_CMD}rm -rf "${WORKING_DIR}/Heroku"
+runout ${SUDO_CMD}git clone https://github.com/coddrago/Heroku/ "${WORKING_DIR}/Heroku" || {
 	errorout "Clone failed."
 	exit 3
 }
-cd "${WORKING_DIR}/Hikka" || {
+cd "${WORKING_DIR}/Heroku" || {
 	printf "\r\033[0;33mRun: \033[1;33mpkg install git\033[0;33m and restart installer"
 	exit 7
 }
@@ -91,8 +71,7 @@ cd "${WORKING_DIR}/Hikka" || {
 printf "\r\033[K\033[0;32mRepo cloned!\e[0m"
 printf "\n\r\033[0;34mCreating config.json...\e[0m"
 
-# Создаем файл config.json внутри папки Hikka
-cat > "${WORKING_DIR}/Hikka/config.json" <<EOL
+cat > "${WORKING_DIR}/Heroku/config.json" <<EOL
 {
     "api_id": 7301124,
     "api_hash": "46eb50618e7a00ca8b165275d9e0fe0b",
@@ -103,15 +82,13 @@ EOL
 printf "\r\033[K\033[0;32mconfig.json created!\e[0m"
 printf "\n\r\033[0;34mInstalling python dependencies...\e[0m"
 
-# shellcheck disable=SC2086
 runin "$SUDO_CMD python$PYVER" -m pip install --upgrade pip setuptools wheel --user
-# shellcheck disable=SC2086
 runin "$SUDO_CMD python$PYVER" -m pip install -r requirements.txt --upgrade --user --no-warn-script-location --disable-pip-version-check || {
 	errorin "Requirements failed!"
 	exit 4
 }
-rm -f "${WORKING_DIR}/hikka-install.log"
+rm -f "${WORKING_DIR}/heroku-install.log"
 touch "${WORKING_DIR}/.setup_complete"
 
 printf "\r\033[K\033[0;32mDependencies installed!\e[0m"
-printf "\n\033[0;32mHikka installation complete! To start it, run: python$PYVER -m hikka\e[0m\n\n"
+printf "\n\033[0;32mHeroku installation complete! To start it, run: python$PYVER -m heroku\e[0m\n\n"
