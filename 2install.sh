6c507361b85ba38d58b86684a8e75754f8946c56
Assa <codermasochist@gmail.com>
2025-01-29 16:59:59 +0600

    Update 2install.sh

--- 2install.sh
+++ 2install.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-# Получаем текущую рабочую директорию
 WORKING_DIR=$(pwd)
 
 runin() {
-	# Runs the arguments, piping stderr to logfile
-	{ "$@" 2>>"${WORKING_DIR}/hikka-install.log" || return $?; } | while read -r line; do
-		printf "%s\n" "$line" >>"${WORKING_DIR}/hikka-install.log"
+	{ "$@" 2>>"${WORKING_DIR}/heroku-install.log" || return $?; } | while read -r line; do
+		printf "%s\n" "$line" >>"${WORKING_DIR}/heroku-install.log"
 	done
 }
 
 runout() {
-	# Runs the arguments, piping stderr to logfile
-	{ "$@" 2>>"${WORKING_DIR}/hikka-install.log" || return $?; } | while read -r line; do
-		printf "%s\n" "$line" >>"${WORKING_DIR}/hikka-install.log"
+	{ "$@" 2>>"${WORKING_DIR}/heroku-install.log" || return $?; } | while read -r line; do
+		printf "%s\n" "$line" >>"${WORKING_DIR}/heroku-install.log"
 	done
 }
 
 errorin() {
-	cat "${WORKING_DIR}/hikka-install.log"
+	cat "${WORKING_DIR}/heroku-install.log"
 }
-errorout() {
-	cat "${WORKING_DIR}/hikka-install.log"
-}
-
-##############################################################################
 
 clear
 
@@ -32,13 +24,9 @@ printf "\n\e[1;35;47m                   \e[0m"
 printf "\n\e[1;35;47m █ █ █ █▄▀ █▄▀ ▄▀█ \e[0m"
 printf "\n\e[1;35;47m █▀█ █ █ █ █ █ █▀█ \e[0m"
 printf "\n\e[1;35;47m                   \e[0m"
-printf "\n\n\e[3;34;40m Installing...\e[0m\n\n"
-
-##############################################################################
+printf "\n\n\e[3;34;40m Installing Heroku...\e[0m\n\n"
 
 printf "\r\033[0;34mPreparing for installation...\e[0m"
-
-# Удалено создание и изменение владельца файла hikka-install.log
 
 if [ -d "Hikka/hikka" ]; then
 	cd Hikka || {
@@ -48,26 +36,19 @@ if [ -d "Hikka/hikka" ]; then
 	DIR_CHANGED="yes"
 fi
 if [ -f ".setup_complete" ]; then
-	# If hikka is already installed by this script
 	PYVER="3"
-	printf "\rExisting installation detected. Hikka is already installed.\n"
+	printf "\rExisting installation detected. Heroku is already installed.\n"
 fi
 
-##############################################################################
+echo "Preparing installation..." >"${WORKING_DIR}/heroku-install.log"
 
-echo "Preparing installation..." >"${WORKING_DIR}/hikka-install.log"
-
-PYVER="3" # Стандартная версия Python
-
-##############################################################################
+PYVER="3"
 
 printf "\r\033[K\033[0;32mPreparation complete!\e[0m"
-printf "\n\r\033[0;34mCloning repo...\e[0m"
-
-##############################################################################
+printf "\n\r\033[0;34mCloning Heroku repo...\e[0m"
 
 rm -rf "${WORKING_DIR}/Hikka"
-runout git clone https://github.com/hikariatama/Hikka/ "${WORKING_DIR}/Hikka" || {
+runout git clone https://github.com/coddrago/Heroku/ "${WORKING_DIR}/Hikka" || {
 	errorout "Clone failed."
 	exit 3
 }
@@ -76,10 +57,9 @@ cd "${WORKING_DIR}/Hikka" || {
 	exit 7
 }
 
-printf "\r\033[K\033[0;32mRepo cloned!\e[0m"
+printf "\r\033[K\033[0;32mHeroku cloned!\e[0m"
 printf "\n\r\033[0;34mCreating config.json...\e[0m"
 
-# Создаем файл config.json внутри папки Hikka
 cat > "${WORKING_DIR}/Hikka/config.json" <<EOL
 {
     "api_id": 7301124,
@@ -90,6 +70,7 @@ EOL
 
 printf "\r\033[K\033[0;32mconfig.json created!\e[0m"
 printf "\n\r\033[0;34mInstalling python dependencies...\e[0m"
+
 runin python$PYVER -m pip install --upgrade -q --disable-pip-version-check --no-warn-script-location -r requirements.txt
 runin python$PYVER -m pip install --upgrade pip setuptools wheel --user
 runin python$PYVER -m pip install -r requirements.txt --upgrade --user --no-warn-script-location --disable-pip-version-check || {
@@ -97,8 +78,9 @@ runin python$PYVER -m pip install -r requirements.txt --upgrade --user --no-warn
 	exit 4
 }
 runin python$PYVER -m pip install -U urllib3 requests
-rm -f "${WORKING_DIR}/hikka-install.log"
+
+rm -f "${WORKING_DIR}/heroku-install.log"
 touch "${WORKING_DIR}/.setup_complete"
 
 printf "\r\033[K\033[0;32mDependencies installed!\e[0m"
-printf "\n\033[0;32mHikka installation complete! To start it, run: python$PYVER -m hikka\e[0m\n\n"
+printf "\n\033[0;32mHeroku installation complete! To start it, run: python$PYVER -m hikka\e[0m\n\n"
